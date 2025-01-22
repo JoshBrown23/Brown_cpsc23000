ebccefd6a2e950861f854e7e65b942ce58aca6ec
Josh Brown <pokeboy99323@gmail.com>
2025-01-22 14:37:45 -0600

    Add project files.

---
+++ NET_Programming_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NET_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            float x;
+            x = Console.Read();
+            Console.WriteLine(x == 0 ? "number not set" : x);
+        }
+    }
+}
