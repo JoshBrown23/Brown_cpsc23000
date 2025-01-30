8117b2304cf31d0515a74685c3e3a3e16a1c9fd6
Josh Brown <pokeboy99323@gmail.com>
2025-01-29 19:07:43 -0600

    Adding week 2 example Sum the Numbers

---
+++ SumTheNumbers/SumTheNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿/*use a for loop to sum up all the numbers up to the number entered by the user.*/
+using static System.Console;
+namespace SumTheNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cap;
+            Write("Enter the number you want to sum up to... ");
+            cap = int.Parse(ReadLine());
+            int sum = 0;
+            for (int i = 0; i <= cap; i++) {
+                sum = sum + i;
+            }
+            WriteLine($"The sum is {sum}.");
+        }
+    }
+}
