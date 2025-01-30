91ecb12b6c10ffc035f185858caf4484034b2e96
Josh Brown <pokeboy99323@gmail.com>
2025-01-29 18:43:04 -0600

    Adding week 2 example Even and Odd projects

---
+++ EvenOddWithIf/EvenOddWithIf/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+/* This simple program asks the user for an integer and then determines 
+ * if it is even or odd */
+namespace EvenOddWithIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Write("Enter an integer: ");
+            try
+            {
+                number = int.Parse(ReadLine());
+            }
+            catch (FormatException)
+            {
+                WriteLine("You had to enter an integer.");
+                return;
+            }
+
+            if (number % 2 == 0)
+            {
+                WriteLine($"THe number {number} is even.");
+            } 
+            else {
+                WriteLine($"The number {number} is odd.");
+            }
+
+        }
+    }
+}
