e7847e0e33e9291a6ea54becb6b925213a6268c6
Josh Brown <pokeboy99323@gmail.com>
2025-01-29 18:54:43 -0600

    Adding week 2 example Grade Commentor

---
+++ GradeCommentor/GradeCommentor/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+namespace GradeCommentor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter your letter grade: ");
+            string letterGrade = ReadLine().ToUpper();
+            switch (letterGrade) 
+            {
+                case "A":
+                    WriteLine("Congratulations! You're pretty smart.");
+                    break;
+                case "B":
+                    WriteLine("Good job. Keep it up.");
+                    break;
+                case "C":
+                    WriteLine("C is for cookie, and cookies are delicious.");
+                    break;
+                case "D":
+                    WriteLine("D is for diploma.");
+                    break;
+                case "F":
+                    WriteLine("Mom and Dad are going to be mad.");
+                    break;
+                default:
+                    WriteLine("You're so dumb.");
+                    break;
+            }
+
+        }
+    }
+}
+
